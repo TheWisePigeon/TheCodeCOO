c7d41aeea4b5d2048a8deae3fb58338d92f3a453
TheWisePigeon <zozozozeph@gmail.com>
2022-12-10 22:13:23 +0000

    feat: added logo and description

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<!-- Header -->
+<div class=" bg-clip-text text-transparent from-green-300 via-blue-500 to-purple-600 bg-gradient-to-tr  text-xs px-5 mt-10 flex justify-between items-baseline" >
+    <h1 class="  text-xl " >T C C</h1>
+    <h1>Subscribe</h1>
+</div>
+
+<!-- Logo -->
+<div class=" flex flex-col items-center justify-center my-8" >
+    <div class=" w-4/5 h-40 bg-[url('logo.png')] bg-no-repeat bg-contain bg-center" >
+
+    </div>
+    <h1 class=" text-xs text-center" >Your weekly dose of web dev content straight from the coop</h1>
+</div>
