4d7359b3d716bc20026f2bd2423d24ef528a554f
TheWisePigeon <zozozozeph@gmail.com>
2022-12-22 21:02:03 +0000

    started design over again

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,6 @@
     import "../app.css"
 </script>
 
-<header class=" sticky top-0 w-full flex justify-between p-2 text-lg backdrop-blur-lg "  >
-    <h1 >TheCodeCOO</h1>
-    <div class=" flex gap-10" >
-        <a href="/posts">
-            <h1>Posts</h1>
-        </a>
-        <a href="/about">
-            <h1>About</h1>
-        </a>
-    </div>
-</header>
-<slot/>
+<main class=" w-[50%]  m-auto">
+    <slot/>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,8 +11,21 @@
     // })
 </script>
 
-<main class=" h-screen" >
-    <div class=" text-center " >
-        <h1>Your weekly dose of web technology, great memes and news of the techspace straight from the coop</h1>
+
+<header class=" flex justify-between items-center ">
+    <a class=" font-bold text-2xl flex" href="/" >
+        The Code Coo
+        <img src="/pigeon.png" alt="" class=" w-10 h-10 animate-bounce">
+    </a>
+    <div class=" flex gap-10" >
+        <a class=" hover:underline font-semibold" href="/subscribe" >
+            <h1>Subscribe</h1>
+        </a>
+        <a class=" hover:underline font-semibold" href="/about" >
+            <h1>About</h1>
+        </a>
     </div>
+</header>
+<main class=" h-screen" >
+    
 </main>
